c85ae643966c6b6e699402909198e01fc4fb7328
sami purmonen <purmonen@kth.se>
2014-10-17 10:48:54 +0200

    third

--- AvalgHash/main.cpp
+++ AvalgHash/main.cpp
@@ -42,13 +42,13 @@ uint64_t hash(uint64_t x) {
     return result;
 }
 
-uint64_t secondHash(uint64_t x, uint64_t seed, uint64_t size) {
+uint64_t secondHash(uint64_t x, uint64_t seed, uint64_t bits) {
     srand(int(seed));
     uint64_t result = 0;
-    for (uint64_t i = 0; i < size; i++) {
+    for (uint64_t i = 0; i < bits; i++) {
         result += ((bitsSet(randomNum() & x)) % 2) << i;
     }
-    return result % size;
+    return result;
 }
 
 bool hashContains(uint64_t x) {
@@ -56,7 +56,7 @@ bool hashContains(uint64_t x) {
     if (hashTable[i].size() < 3) {
         return find(hashTable[i].begin(), hashTable[i].end(), x) != hashTable[i].end();
     }
-    return x == hashTable[i][secondHash(x, seeds[i], hashTable[i].size())];
+    return x == hashTable[i][secondHash(x, seeds[i], hashTable[i].size()) % hashTable[i].size()];
 }
 
 
@@ -66,7 +66,7 @@ int main(int argc, const char * argv[]) {
         H[i] = randomNum();
     }
     std::cout << "Reading file" << std::endl;
-    std::ifstream fileStream("avalghash");
+    std::ifstream fileStream("/Users/Sami/Desktop/avalghash");
     uint64_t num;
     std::vector<uint64_t> nums;
     while (fileStream >> std::hex >> num) {
@@ -77,10 +77,11 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hashing elements" << std::endl;
     int count = 0;
     int empty = 0;
+    std::vector<int> usedSeeds[30];
     for (int i = 0; i < slots; i++) {
         uint64_t currentSize = hashTable[i].size();
         if (currentSize > 3) {
-//            std::cout << "Hashing " << i << ", size " << currentSize << std::endl;
+            //            std::cout << "Hashing " << i << ", size " << currentSize << std::endl;
         }
         if (currentSize == 0) {
             empty++;
@@ -88,11 +89,18 @@ int main(int argc, const char * argv[]) {
         if (currentSize >= 3) {
             count++;
             auto seed = seeds[i];
+            int bits = ceil(log2(currentSize));
+            auto remainingSeeds = usedSeeds[bits];
             while (seeds[i] == 0) {
                 seed = int(randomNum());
+                if (remainingSeeds.size() > 0) {
+                    seed = remainingSeeds[remainingSeeds.size()-1];
+                    remainingSeeds.pop_back();
+                }
+                
                 std::vector<uint64_t> hashes;
                 for (auto num: hashTable[i]) {
-                    auto h = secondHash(num, seed, currentSize) ;
+                    auto h = secondHash(num, seed, currentSize) % currentSize;
                     if (std::find(hashes.begin(), hashes.end(), h) != hashes.end()) {
                         break;
                     }
@@ -106,12 +114,22 @@ int main(int argc, const char * argv[]) {
                         j++;
                     }
                     seeds[i] = seed;
-                    bits[i] = ceil(log2(currentSize));
+                    if (find(usedSeeds[bits].begin(), usedSeeds[bits].end(), seed) == usedSeeds[bits].end()) {
+                        usedSeeds[bits].push_back(seed);
+                    }
+                    //                    bits[i] = bits;
                     hashTable[i] = nums;
                 }
             }
         }
     }
+    
+    int numSeedsUsed = 0;
+    for (int i = 0; i < 20; i++) {
+        numSeedsUsed += usedSeeds[i].size();
+    }
+    std::cout << "Collisions >= 3 " << count << std::endl;
+    std::cout << "Used inner matrices " << numSeedsUsed << std::endl;
     std::cout << "The matrix" << std::endl;
     for (int i = 0; i < rows; i++) {
         auto x = H[i];
@@ -120,14 +138,16 @@ int main(int argc, const char * argv[]) {
         }
         std::cout << std::endl;
     }
-
-    for (int i = 0; i < slots; i++) {
-        if (hashTable[i].size() >= 3) {
-            std::cout << "H" << i << ", bits " << bits[i] << std::endl;
-
-            srand(seeds[i]);
+    int j = 0;
+    for (int i = 0; i < 20; i++) {
+        if (usedSeeds[i].size() == 0) {
+            continue;
+        }
+        for (auto seed: usedSeeds[i]) {
+            std::cout << "H" << j << ", bits " << i << std::endl;
+            srand(seed);
             std::vector<uint64_t> nums;
-            for (uint64_t j = 0; j < bits[i]; j++) {
+            for (uint64_t j = 0; j < i; j++) {
                 nums.push_back(randomNum());
             }
             for (auto x: nums) {
@@ -136,17 +156,18 @@ int main(int argc, const char * argv[]) {
                 }
                 std::cout << std::endl;
             }
+            j++;
         }
     }
     
-//    std::cout << "Looking if we found elements " << std::endl;
-//    for (auto num: nums) {
-//        if (!hashContains(num)) {
-//            std::cout << "Num not found " << num << std::endl;
+//        std::cout << "Looking if we found elements " << std::endl;
+//        for (auto num: nums) {
+//            if (!hashContains(num)) {
+//                std::cout << "Num not found " << num << std::endl;
+//            }
 //        }
-//    }
-//    std::cout << 1337 << " " << hashContains(1337) << std::endl;
-//    std::cout << "Atleast " << count << " hash functions must be made" << std::endl;
-//    std::cout << "Empty " << empty << std::endl;
+//        std::cout << 1337 << " " << hashContains(1337) << std::endl;
+//        std::cout << "Atleast " << count << " hash functions must be made" << std::endl;
+//        std::cout << "Empty " << empty << std::endl;
     return 0;
 }
